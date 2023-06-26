ccaadb6363fa162606907d2abcc72dccf970c80b
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-06-26 11:47:06 +0200

    Create Food class, add functionality for increasing health and energy

--- evolution.py
+++ evolution.py
@@ -9,6 +9,7 @@ HEIGHT = 400
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
+GREEN = (0, 255, 0)
 
 # Define the Cell class
 class Cell:
@@ -18,10 +19,34 @@ class Cell:
         self.size = 50
         self.position = [WIDTH // 2, HEIGHT // 2]
 
-    def update(self):
-        self.health -= random.randint(1, 10)
-        self.energy -= random.randint(1, 10)
-        self.size = max(0, self.size - random.randint(1, 5))
+    def update(self, food_list):
+        self.health -= random.randint(1, 5)
+        self.energy -= random.randint(1, 5)
+        self.size = max(5, self.size - random.randint(1, 2))
+
+        if len(food_list) > 0:
+            closest_food = min(food_list, key=lambda food: self.distance_to_food(food))
+            if self.distance_to_food(closest_food) > 0:
+                self.move_towards_food(closest_food)
+
+            if self.is_touching_food(closest_food):
+                self.health += closest_food.size
+                self.energy += closest_food.size
+                self.size += random.randint(1, 2)
+                food_list.remove(closest_food)
+
+    def distance_to_food(self, food):
+        return ((self.position[0] - food.position[0]) ** 2 + (self.position[1] - food.position[1]) ** 2) ** 0.5
+
+    def move_towards_food(self, food):
+        dx = food.position[0] - self.position[0]
+        dy = food.position[1] - self.position[1]
+
+        distance = self.distance_to_food(food)
+        speed = min(distance, 10)
+
+        self.position[0] += int(speed * dx / distance)
+        self.position[1] += int(speed * dy / distance)
 
     def is_alive(self):
         return self.health > 0 and self.energy > 0
@@ -34,6 +59,20 @@ class Cell:
         pygame.draw.rect(screen, RED, (self.position[0] - self.size // 2, self.position[1] - self.size // 2,
                                        self.size, self.size))
 
+    def is_touching_food(self, food):
+        distance = self.distance_to_food(food)
+        return distance < (self.size + food.size) / 2
+
+# Define the Food class
+class Food:
+    def __init__(self):
+        self.size = random.randint(5, 10)
+        self.position = [random.randint(0, WIDTH), random.randint(0, HEIGHT)]
+        self.color = GREEN
+
+    def display_food(self, screen):
+        pygame.draw.circle(screen, self.color, self.position, self.size)
+
 # Initialize Pygame
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -42,6 +81,7 @@ pygame.display.set_caption("Cell Evolution")
 # Main simulation loop
 def simulate_evolution():
     cell = Cell()
+    food_list = []
 
     clock = pygame.time.Clock()
 
@@ -51,7 +91,12 @@ def simulate_evolution():
                 pygame.quit()
                 return
 
-        cell.update()
+        cell.update(food_list)
+
+        # Create new food randomly
+        if random.random() < 0.45:
+            food = Food()
+            food_list.append(food)
 
         screen.fill(WHITE)
 
@@ -64,6 +109,10 @@ def simulate_evolution():
         screen.blit(text_energy, (20, 60))
 
         cell.display_cell(screen)
+
+        # Display food
+        for food in food_list:
+            food.display_food(screen)
 
         pygame.display.flip()
 
