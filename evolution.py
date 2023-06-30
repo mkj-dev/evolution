b0c3ba586d07c1c7d7462724defd92de4675b2ad
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-06-30 17:44:16 +0200

    Create Generation class, that handles the number of cells in the generation, change cells color to random, add more documentation to the code

--- evolution.py
+++ evolution.py
@@ -8,18 +8,27 @@ HEIGHT = 400
 # Define colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 
 # Define the Cell class
 class Cell:
     def __init__(self):
+        """
+        Initialize a cell with default attributes.
+        """
         self.health = 100
         self.energy = 100
         self.size = 50
         self.position = [WIDTH // 2, HEIGHT // 2]
+        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
     def update(self, food_list):
+        """
+        Update the cell's attributes based on its interactions with the environment.
+        
+        Args:
+            food_list (list): List of Food objects in the environment.
+        """
         self.health -= random.randint(1, 5)
         self.energy -= random.randint(1, 5)
         self.size = max(5, self.size - random.randint(1, 2))
@@ -36,9 +45,24 @@ class Cell:
                 food_list.remove(closest_food)
 
     def distance_to_food(self, food):
+        """
+        Calculate the distance between the cell and a food object.
+        
+        Args:
+            food (Food): The Food object.
+        
+        Returns:
+            float: The distance between the cell and the food object.
+        """
         return ((self.position[0] - food.position[0]) ** 2 + (self.position[1] - food.position[1]) ** 2) ** 0.5
 
     def move_towards_food(self, food):
+        """
+        Move the cell towards a food object.
+        
+        Args:
+            food (Food): The Food object.
+        """
         dx = food.position[0] - self.position[0]
         dy = food.position[1] - self.position[1]
 
@@ -49,112 +73,190 @@ class Cell:
         self.position[1] += int(speed * dy / distance)
 
     def is_alive(self):
+        """
+        Check if the cell is alive based on its health and energy.
+        
+        Returns:
+            bool: True if the cell is alive, False otherwise.
+        """
         return self.health > 0 and self.energy > 0
 
-    def display_info(self):
-        print("Health:", self.health)
-        print("Energy:", self.energy)
-
-    def display_cell(self, screen):
-        pygame.draw.rect(screen, RED, (self.position[0] - self.size // 2, self.position[1] - self.size // 2,
-                                       self.size, self.size))
-
     def is_touching_food(self, food):
+        """
+        Check if the cell is touching a food object.
+        
+        Args:
+            food (Food): The Food object.
+        
+        Returns:
+            bool: True if the cell is touching the food object, False otherwise.
+        """
         distance = self.distance_to_food(food)
         return distance < (self.size + food.size) / 2
+
+    def display(self, screen):
+        """
+        Display the cell on the screen.
+        
+        Args:
+            screen (pygame.Surface): The pygame surface representing the screen.
+        """
+        pygame.draw.rect(screen, self.color, (self.position[0] - self.size // 2, self.position[1] - self.size // 2, self.size, self.size))
 
 # Define the Food class
 class Food:
     def __init__(self):
+        """
+        Initialize a food object with random attributes.
+        """
         self.size = random.randint(5, 10)
         self.position = [random.randint(0, WIDTH), random.randint(0, HEIGHT)]
         self.color = GREEN
 
     def display_food(self, screen):
+        """
+        Display the food object on the screen.
+        
+        Args:
+            screen (pygame.Surface): The pygame surface representing the screen.
+        """
         pygame.draw.circle(screen, self.color, self.position, self.size)
 
-# Initialize Pygame
-pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Cell Evolution")
+# Define the Generation class
+class Generation:
+    def __init__(self, population_size):
+        """
+        Initialize a generation of cells.
+        
+        Args:
+            population_size (int): The number of cells in the generation.
+        """
+        self.population_size = population_size
+        self.cells = []
+        self.food_list = []
+        self.timer = 0
+        self.generation_count = 0
 
-# Main simulation loop
-def simulate_evolution():
-    cell = Cell()
-    clock = pygame.time.Clock()
-    food_list = []
-    timer = 0
+        for _ in range(self.population_size):
+            cell = Cell()
+            self.cells.append(cell)
 
-    while cell.is_alive():
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                return
+    def update(self):
+        """
+        Update the generation by updating each cell and handling reproduction.
+        """
+        for cell in self.cells:
+            cell.update(self.food_list)
 
-        cell.update(food_list)
-        timer += 1
+        self.timer += 1
 
         # Create new food randomly
         if random.random() < 0.25:
             food = Food()
-            food_list.append(food)
+            self.food_list.append(food)
 
-        screen.fill(WHITE)
+        self.cells = [cell for cell in self.cells if cell.is_alive()]
 
-        # Display cell information
-        font = pygame.font.Font(None, 18)
-        text_health = font.render("Health: {}".format(cell.health), True, BLACK)
-        text_energy = font.render("Energy: {}".format(cell.energy), True, BLACK)
-        text_timer = font.render("Timer: {}".format(timer), True, BLACK)
+        if self.generation_count == 0 or len(self.cells) == 0:
+            self.reproduce_cells()
+
+    def reproduce_cells(self):
+        """
+        Reproduce cells to create the next generation.
+        """
+        if len(self.cells) == 0:
+            self.cells = [Cell() for _ in range(self.population_size)]
+        else:
+            parents = random.sample(self.cells, k=min(2, len(self.cells)))
+
+            if len(parents) > 0:  # Check if parents are available
+                new_generation = []
+
+                for _ in range(self.population_size):
+                    parent = random.choice(parents)
+                    child = Cell()
+
+                    # Inherit properties from the parent with slight variations
+                    child.health = max(0, min(200, random.gauss(parent.health, 10)))
+                    child.energy = max(0, min(200, random.gauss(parent.energy, 10)))
+                    child.size = max(5, min(100, random.gauss(parent.size, 5)))
+
+                    new_generation.append(child)
+
+                self.cells = new_generation
+        self.generation_count += 1
+
+    def display_info(self, screen):
+        """
+        Display generation information on the screen.
         
-        screen.blit(text_health, (20, 20))
-        screen.blit(text_energy, (100, 20))
+        Args:
+            screen (pygame.Surface): The pygame surface representing the screen.
+        """
+        font = pygame.font.Font(None, 18)
+        text_generation = font.render("Generation: {}".format(self.generation_count), True, BLACK)
+        text_timer = font.render("Timer: {}".format(self.timer), True, BLACK)
+
+        screen.blit(text_generation, (20, 20))
         screen.blit(text_timer, (180, 20))
 
-        cell.display_cell(screen)
+    def display_cells(self, screen):
+        """
+        Display the cells in the generation on the screen.
+        
+        Args:
+            screen (pygame.Surface): The pygame surface representing the screen.
+        """
+        for cell in self.cells:
+            cell.display(screen)
 
-        # Display food
-        for food in food_list:
+    def display_food(self, screen):
+        """
+        Display the food objects in the environment on the screen.
+        
+        Args:
+            screen (pygame.Surface): The pygame surface representing the screen.
+        """
+        for food in self.food_list:
             food.display_food(screen)
 
-        pygame.display.flip()
+    def run_simulation(self):
+        """
+        Run the simulation.
+        """
+        pygame.init()
+        screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption("Cell Evolution")
 
-        clock.tick(1)  # Set the desired frame rate
+        clock = pygame.time.Clock()
 
-    # Create an ending screen
-    screen.fill(WHITE)
-
-    # Display game over message
-    font_large = pygame.font.Font(None, 48)
-    text_game_over = font_large.render("Game Over", True, BLACK)
-    text_game_over_rect = text_game_over.get_rect(center=(WIDTH // 2, HEIGHT // 2 - 50))
-    screen.blit(text_game_over, text_game_over_rect)
-
-    # Display score for the cell
-    text_timer = font.render("Your Cell lasted {} seconds.".format(timer), True, BLACK)
-    screen.blit(text_timer, (WIDTH // 2 - 80, HEIGHT // 2))
-
-    # Display restart button
-    font_small = pygame.font.Font(None, 28)
-    text_restart = font_small.render("Restart", True, BLACK)
-    text_restart_rect = text_restart.get_rect(center=(WIDTH // 2, HEIGHT // 2 + 50))
-    pygame.draw.rect(screen, GREEN, (text_restart_rect.x - 10, text_restart_rect.y - 5,
-                                     text_restart_rect.width + 20, text_restart_rect.height + 10))
-    screen.blit(text_restart, text_restart_rect)
-
-    pygame.display.flip()
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                return
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_pos = pygame.mouse.get_pos()
-                if text_restart_rect.collidepoint(mouse_pos):
-                    # Restart the simulation
-                    simulate_evolution()
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
                     return
 
-# Run the simulation
-simulate_evolution()
+            screen.fill(WHITE)
+
+            self.update()
+
+            self.display_info(screen)
+            self.display_cells(screen)
+            self.display_food(screen)
+
+            pygame.display.flip()
+
+            clock.tick(30)  # Set the desired frame rate
+
+
+# Define the main function
+def main():
+    """
+    The main function that initializes a generation and runs the simulation.
+    """
+    generation = Generation(population_size=3)
+    generation.run_simulation()
+
+# Run the main function
+if __name__ == "__main__":
+    main()
